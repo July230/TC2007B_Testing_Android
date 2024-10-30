9b783dc8a01e49cd5fce5d0b7e69c724d02bcad7
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-10-29 19:32:13 -0600

    Innitial commit

---
+++ app/src/test/java/com/example/myapplication/UserClassTest.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class UserClassTest {
+
+    @Test
+    fun isAdult() {
+        val user = UserClass("Julian", 19)
+        assertTrue(user.isAdult())
+    }
+
+    @Test
+    fun isTeenager() {
+        val user = UserClass("Julian", 15)
+        assertTrue(user.isTeenager())
+    }
+
+    @Test
+    fun isChild() {
+        val user = UserClass("Julian", 15)
+        assertTrue(user.isChild())
+    }
+}
